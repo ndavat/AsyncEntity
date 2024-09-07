8ca88a22eb53e9c7bdf5dd69d32b891af83c04c1
runneradmin <runneradmin@fv-az1400-218>
2024-09-07 02:28:32 +0000

    Add project files.

---
+++ AsyncEntity/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncEntity
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ AsyncEntity/DataInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncEntity
+{
+    public class DataInitializer
+    {
+        private readonly DataContext _db;
+        public DataInitializer(DataContext db) 
+        {
+            _db = db;
+        }
+        public async Task Run()
+        {
+            await _db.Database.EnsureDeletedAsync();
+            await _db.Database.EnsureCreatedAsync();
+            var total = 1000000;
+            for (int i = 0; i < total; i++)
+            {
+                _db.Suppliers.Add(new Supplier 
+                {
+                    Name = $"P{(i+total)}"
+                });
+                
+                _db.Customers.Add(new Customer 
+                {
+                    Name = $"C{(i+total)}"
+             
+                });
+            }
+            await _db.SaveChangesAsync();
+        }
+    }
+}
